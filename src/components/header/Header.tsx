cdbfd769f09e5f922a33b9e268a96208f16eb30d
Guevaer Arnaud <guevaer8@gmail.com>
2025-10-17 14:33:37 +0200

    adding input in the header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,6 +4,16 @@ export default function App() {
   return (
     <header className="header">
       <img className="header-logo" src="src\assets\logo.png" alt="Logo" />
+      <form className="header-form">
+        <input
+          className="header-searchbar"
+          type="search"
+          placeholder="Rechercher un film ou acteur..."
+        />
+        <button className="header-searchbar-button" type="submit">
+          🔍
+        </button>
+      </form>
     </header>
   );
 }
